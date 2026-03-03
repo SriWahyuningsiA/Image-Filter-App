28c64643c1b32568ce05a1889179361f9994f2b9
SriWahyuningsiA <swhyuni34@gmail>
2026-03-03 19:05:44 +0800

    Update filter dan tampilan CSS

--- src/FilterControls.js
+++ src/FilterControls.js
@@ -12,13 +12,13 @@ function FilterControls({
     <div className="filter-box">
       <h2>Filtering</h2>
 
-      <div className="control">
-        <label>Grayscale</label>
-        <input
-          type="checkbox"
-          checked={grayscale}
-          onChange={() => setGrayscale(!grayscale)}
-        />
+      <div className="control grayscale-control">
+  <label>Grayscale</label>
+  <input
+    type="checkbox"
+    checked={grayscale}
+    onChange={() => setGrayscale(!grayscale)}
+  />
       </div>
 
       <div className="control">
